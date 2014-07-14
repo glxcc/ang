8992b616d747afa2525f8cf6ed647ac2292bb354
nlg <guolinxin@gmail.com>
2014-07-14 15:47:32 +0100

    chap 1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+function readJSONFile(filename, callback) {
+    fs.readFile(filename, function (err, data) {
+        if(err) {
+            callback(err);
+            return;
+        }
+        try {
+
+            var dataString = data.toString();
+            console.log(dataString);
+
+           // callback(null, JSON.parse(dataString));
+            callback(null, dataString);
+        } catch(exception) {
+            callback(exception);
+        }
+    });
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,6 +29,24 @@ router.get('/', function(req, res) {
 router.get('/todo', function(req, res, next){
 
     res.render('todo', {title : 'Todo'})
+})
+
+router.get('/ajax/:file', function(req, res , next){
+    //  var path = require('path'),
+    // appDir = path.dirname(require.main.filename);
+   var filePath = __dirname + '\\files\\'+  req.params.file
+    readJSONFile(filePath, function (err, json) {
+        if(err) { throw err; }
+        console.log(json);
+        res.send(json)
+    });
+
+  //  res.send();
+
+})
+
+router.get('/ajax', function(req, res , next){
+    res.send("called");
 })
 
 
